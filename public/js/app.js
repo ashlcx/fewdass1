a5364b315979b10c7ab60c91f46503ceecd8ea44
Ash Hines <ahines@ch-earnest.com>
2022-04-12 14:06:27 +1000

    add some pages in

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 console.log("App Started");
 var theme = "light";
-//loadBackground();
+loadBackground();
 detectColorScheme();
 
 const toggleColourTheme = () => {
@@ -17,7 +17,7 @@ const loadParallax_homePage = () => {
   $("#hero-image").parallax({
     imageSrc:
       "https://images.squarespace-cdn.com/content/v1/5e142229c73726304407e983/1647989567154-SX40FIANGY7BUHDEQZUS/HQ+landscape+ducks+dawn.jpg?format=2500w",
-    naturalWidth: "1667",
+    naturalWidth: "1667", //TODO Update values
     naturalHeight: "2500",
   });
   $("#parallax-1").parallax({
@@ -81,6 +81,11 @@ window.addEventListener("load", () => {
   const errorTemplate = Handlebars.compile($("#error-template").html());
   const homeTemplate = Handlebars.compile($("#home-template").html());
   const donateTemplate = Handlebars.compile($("#donate-template").html());
+  const covidTemplate = Handlebars.compile($("#covid-template").html());
+  const festival22Template = Handlebars.compile(
+    $("#festival-22-template").html()
+  );
+  const aboutTemplate = Handlebars.compile($("#aboutus-template").html());
 
   const router = new Router({
     mode: "history",
@@ -99,6 +104,25 @@ window.addEventListener("load", () => {
   router.add("/donate", () => {
     let html = donateTemplate();
     app.html(html);
+    jQuery(window).trigger("resize").trigger("scroll");
+  });
+
+  router.add("/covid-19", () => {
+    let html = covidTemplate();
+    app.html(html);
+    jQuery(window).trigger("resize").trigger("scroll");
+  });
+
+  router.add("/festivals/2022", () => {
+    let html = festival22Template();
+    app.html(html);
+    jQuery(window).trigger("resize").trigger("scroll");
+  });
+
+  router.add("/about-us", () => {
+    let html = aboutTemplate();
+    app.html(html);
+    jQuery(window).trigger("resize").trigger("scroll");
   });
 
   router.navigateTo(window.location.pathname);
@@ -120,5 +144,6 @@ window.addEventListener("load", () => {
     const href = target.attr("href");
     const path = href.substr(href.lastIndexOf("/"));
     router.navigateTo(path);
+    jQuery(window).trigger("resize").trigger("scroll");
   });
 });
