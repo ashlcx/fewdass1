1e20abe2f9245217e8e342324b0498308e539183
Ash Hines <ahines@ch-earnest.com>
2022-04-13 11:22:18 +1000

    Add about page

--- public/js/app.js
+++ public/js/app.js
@@ -72,7 +72,23 @@ const toggleMobileMenu = () => {
   toggled.toggle("slow");
 };
 
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 window.addEventListener("load", () => {
+  mybutton = document.getElementById("toTopButton");
+
+window.onscroll = function() {scrollFunction()};
   $("#hamburger").click(() => {
     $("#nav-links").toggle("fast");
   });
